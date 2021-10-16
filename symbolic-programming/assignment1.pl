0f3b7dfa967f1e365f99e56a87cf9843fa5af175
Alice Doherty <66687032+alicedoherty@users.noreply.github.com>
2021-10-16 22:35:33 +0100

    symbolic ex 2 working

--- symbolic-programming/assignment1.pl
+++ symbolic-programming/assignment1.pl
@@ -1,7 +1,10 @@
+:- discontiguous simplify/3.
+:- discontiguous add/3.
 
 numeral(0).
 numeral(s(X)) :- numeral(X).
 numeral(X+Y) :- numeral(X), numeral(Y).
+numeral(p(X)) :- numeral(X).
 
 add(0,X,X).
 add(s(X),Y,s(Z)) :- add(X,Y,Z).
@@ -14,13 +17,6 @@ add(s(X),Y,s(Z)) :- add(X,Y,Z).
 ?- add2(s(0)+s(0), s(0+s(s(0))), Z).
 */
 
-/** First Iteration
-add2(X+A,Y+B,Z) :- add(X,A,R1), add(Y,B,R2), add2(R1,R2,Z).
-add2(X+A,Y,Z) :- add(X,A,R), add2(R,Y,Z).
-add2(X,Y+A,Z) :- add(Y,A,R), add2(X,R,Z).
-add2(X,Y,Z) :- add(X,Y,Z).
-*/
-
 add2(X,Y,Z) :- simplify(X,Y,Z).
 
 simplify(s(X+A),Y,s(Z)) :- simplify(X+A,Y,Z).
@@ -29,5 +25,25 @@ simplify(s(X+A),s(Y+B),s(s(Z))) :- simplify(X+A,Y+B,Z).
 
 simplify(X+A,Y,Z) :- add(X,A,R), add2(R,Y,Z).
 simplify(X,Y+A,Z) :- add(Y,A,R), add2(X,R,Z).
-simplify(X,Y,Z) :- add(X,Y,Z).
 simplify(X+A,Y+B,Z) :- add(X,A,R1), add(Y,B,R2), add2(R1,R2,Z).
+
+% This moved later to get Exercise 2 to work.
+% simplify(X,Y,Z) :- add(X,Y,Z).
+
+% Exercise 2
+/** <examples>
+?- add2(p(s(0)), s(s(0)), Z).
+?- add2(0, s(p(0)), Z).
+?- add2(p(0)+s(s(0)),s(s(0)),Z).
+?- add2(p(0), p(0)+s(p(0)), Z).
+*/
+
+simplify(p(s(X)), Y, Z) :- simplify(X,Y,Z).
+simplify(X, p(s(Y)), Z) :- simplify(X,Y,Z).
+simplify(s(p(X)),Y,Z) :- simplify(X,Y,Z).
+simplify(X, s(p(Y)),Z) :- simplify(X,Y,Z).
+
+add(p(X),Y,p(Z)) :- add(X,Y,Z).
+
+% This was taken from end of Exercise 2
+simplify(X,Y,Z) :- add(X,Y,Z).
